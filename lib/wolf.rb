0f890bdea39506ad23504c202786a62ce7e0b1b4
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-11 12:33:57 +0100

    Commenting to show how it is a bad implementation.

--- lib/wolf.rb
+++ lib/wolf.rb
@@ -1,6 +1,6 @@
 def wolf(stock_price)
   daily_price = stock_price.dup
-  buy_profit = day_to_buy(stock_price)
+  buy_profit = day_to_buy(stock_price) # Can only call day_to_buy once as it changes the array
   buy_day = buy_profit['buy_day']
   profit = buy_profit['max_profit']
   sell_price = daily_price[buy_day] + profit
